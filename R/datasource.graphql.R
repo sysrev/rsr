c5450aec41f820e3519dd74efb7b16f3ed92a0e8
Thomas Luechtefeld <tomluec@gmail.com>
2021-10-18 15:03:05 -0400

    datasource.graphql - better error messaging

--- R/datasource.graphql.R
+++ R/datasource.graphql.R
@@ -2,6 +2,12 @@ datasource.graphql <- function(query,token,.url = "https://datasource.insilica.c
   req <- httr::POST(.url, 
   	body = list(query = query), encode="json", 
   	httr::add_headers(Authorization=paste("Bearer", token)))
-
-  httr::content(req,as = "parsed", encoding = "UTF-8")$data
+  
+  res <- httr::content(req,as = "parsed", encoding = "UTF-8")
+  
+  if(res$behavior == "error"){
+    stop(res$data[[1]]$message)
+  }else{
+    return(res$data)
+  }
 }
