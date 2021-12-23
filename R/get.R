425257a65f28e28678810a90c416ef4cad7390f3
Thomas Luechtefeld <tomluec@gmail.com>
2021-12-23 12:31:37 -0500

    added `get_predictions` and `get_users`

--- R/get.R
+++ R/get.R
@@ -14,7 +14,7 @@ get_articles <- function(pid,token=keyring::key_get("sysrev.token")){
 #' get_article
 #'
 #' get the basic article data from a sysrev project
-#' @param aid the project to get articles from, i.e sysrev.com/p/<project_id>
+#' @param aid the article to get data for
 #' @param token a sysrev token with read access to the given project
 #'
 #' @return A dataframe
@@ -22,6 +22,19 @@ get_articles <- function(pid,token=keyring::key_get("sysrev.token")){
 #'
 get_article <- function(aid,token=keyring::key_get("sysrev.token")){
   rplumber("get_article",list(aid=aid),token) %>% tibble()
+}
+
+#' get_predictions
+#'
+#' get the predictions for a project
+#' @param pid the project to get articles from, i.e sysrev.com/p/<project_id>
+#' @param token a sysrev token with read access to the given project
+#'
+#' @return A dataframe
+#' @export
+#'
+get_predictions <- function(pid,token=keyring::key_get("sysrev.token")){
+  rplumber("get_predictions",list(pid=pid),token) |> tibble()
 }
 
 #' get_labels
@@ -70,6 +83,16 @@ get_labels <- function(pid,token=keyring::key_get("sysrev.token")){
 #' @export
 get_labels_tbl <- function(pid,token=keyring::key_get("sysrev.token")){
   rplumber("get_labels",list(pid=pid),token) |> rename(lbl.id=label_id)|> tibble()
+}
+
+#' get_users
+#' get the users in a project
+#' @import tidyr
+#' @param pid The project identifier.  For sysrev.com/p/3144 the identifier is 3144
+#' @param token a sysrev token with read access to the given project
+#' @export
+get_users <- function(pid,token=keyring::key_get("sysrev.token")){
+  rplumber("get_users",list(pid=pid),token) |> tibble()
 }
 
 #' get_answers
--- R/zzz.R
+++ R/zzz.R
@@ -1,4 +1,3 @@
 .onLoad <- function(libname, pkgname) {
-  cat(glue("loading {libname} {pkgname}"))
   options(rsysrev.sysrev.plumber.url="http://localhost:5216") #TODO lets do this with a config file. See https://github.com/ChadGoymer/githapi/blob/main/R/on-load.R
 }
