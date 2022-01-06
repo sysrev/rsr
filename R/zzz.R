c3f6fb5cfa924cd486c53c183889a3d4f62fe79c
Thomas Luechtefeld <tomluec@gmail.com>
2022-01-05 20:15:19 -0500

    updated srplumber option

--- R/zzz.R
+++ R/zzz.R
@@ -1,3 +1,3 @@
 .onLoad <- function(libname, pkgname) {
-  options(rsr.sysrev.plumber.url="http://localhost:5216") #TODO lets do this with a config file. See https://github.com/ChadGoymer/githapi/blob/main/R/on-load.R
+  options(srplumber.url="https://srplumber.sysrev.com")
 }
