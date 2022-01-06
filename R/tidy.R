91ee6f3b8404d8d013ab83524a74b62dbfccf10f
Thomas Luechtefeld <tomluec@gmail.com>
2022-01-05 20:51:22 -0500

    get_tidy_answers and get_list_answers

--- R/tidy.R
+++ R/tidy.R
@@ -1,3 +1,78 @@
+#' create a tidy version of sysrev answers
+#' @import dplyr
+#' @importFrom rlang .data
+#' @param pid the project-id to export
+#' @param drop.resolved.discordant remove answers for articles that have a resolved alternative?
+#' @return a tbl with tidy sysrev answers
+#' @export
+get_answers_tidy = function(pid,drop.resolved.discordant=F) {
+
+  pco = get_sroptions(pid)
+
+  a = get_answers(pid) |> # get tidy sysrev answers
+    group_by(.data$lid) |> mutate(answer = srtidy_answer(.data$answer, .data$value_type)) |> ungroup() |>
+    group_by(.data$aid) |> # filter out reviews superseded by a resolve review
+    (\(tbl){ purrr::when(drop.resolved.discordant,
+                         T ~ tbl |> filter(.data$resolve == max(.data$resolve)) |> ungroup(),
+                         F ~ tbl )})()
+
+  b = a |> # calculate concordance by aid
+    group_by(.data$aid, .data$lid) |> mutate(conc = concordant(.data$answer, F, pco)) |> ungroup() |>
+    mutate(conc = max(.data$conc, .data$lid %in% pco$consensus.labels)) |>
+    group_by(.data$aid) |> mutate(conc    = all(.data$conc)) |> ungroup() |>
+    group_by(.data$aid) |> mutate(reviews = n_distinct(.data$user_id)) |> ungroup()
+
+  # calculate a concordance status
+  b |> mutate(
+    conc.status = case_when(
+      resolve      ~ "resolved",
+      conc         ~ "concordant",
+      reviews == 1 ~ "single",
+      T            ~ "discordant"
+    ),
+    .keep = "unused"
+  )
+}
+
+#' create a list of tidy answer tibbles
+#' a special `basic` value in the list is populated by categorical boolean and string labels
+#' every other value in the list represents a sysrev group label <rsr_group>
+#' @param pid the project-id to export
+#' @importFrom rlang .data
+#' @importFrom tidyr pivot_wider unnest
+#' @importFrom purrr map_chr map set_names pluck
+#' @return list of tibbles
+#' @export
+get_answers_list = function(pid){
+
+  tidy.ans   = get_answers_tidy(pid)
+
+  basic.tbl  = tidy.ans |>
+    filter(.data$value_type %in% c("categorical","boolean","string")) |>
+    select(-.data$lid,-.data$value_type) |>
+    mutate(answer = map_chr(.data$answer,~paste(.,collapse=";"))) |>
+    pivot_wider(names_from=.data$short_label,values_from = .data$answer)
+
+  lookup.lidname  = get_labels(pid) |>
+    with(\(n){ pluck(short_label,which(lid==n),.default = n) }) |>
+    Vectorize()
+
+  tbls = tidy.ans |>
+    filter(.data$value_type == "group") |>
+    select(-.data$lid,-.data$value_type) |>
+    group_split(.data$short_label) %>%
+    (\(x){ set_names(x,map_chr(x,\(tbl){tbl$short_label[1]})) }) |>
+    map(\(tbl){
+      tbl |>
+        unnest(.data$answer) |>
+        mutate(value=map_chr(.data$value,~paste(.,collapse=";"))) |>
+        pivot_wider(names_from=.data$lid,values_from=.data$value) |>
+        rename_with(lookup.lidname)
+    })
+
+  c(list(basic=basic.tbl),tbls)
+}
+
 tidy.answers.basic   = function(answer){ lapply(answer,jsonparse::from_json) }
 tidy.answers.boolean = function(answer){ lapply(answer,\(x) case_when(x=="true"~T,x=="false"~F,NULL)) }
 
