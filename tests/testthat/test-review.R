3f5c8e9ffeb6f314562be718d2ae507fa4b20323
Thomas Luechtefeld <tom@insilica.co>
2022-01-12 07:59:50 -0500

    basic review test done

--- tests/testthat/test-review.R
+++ tests/testthat/test-review.R
@@ -1,8 +1,16 @@
 
 test_that("setting a label value works", {
-  pid = 105703
-  aid = 13458696
-  lid = "e5772423-7581-4a4a-a0d4-c8c356d2c297"
-  res = review(pid,aid,lid,lval = T)
+  pid  = 105703
+  aid  = 13458696
+  lid  = "e5772423-7581-4a4a-a0d4-c8c356d2c297"
+  uid  = 5804
+  ans = \(){get_answers(pid) |> filter(user_id==uid,lid==lid,aid==aid) |> pull(answer)=="true"}
+
+  ans.pre  = ans()
+  res      = review(pid,aid,lid,lval = !ans.pre)
+  ans.post = ans()
+
+  expect_true(all(is.logical(c(ans.pre,ans.post))))
   expect_true(res$setLabels)
+  expect_true(ans.pre != ans.post)
 })
