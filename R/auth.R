705df6f12eb3cd813b0ad8662c093b549fafe718
Thomas Luechtefeld <tom@insilica.co>
2022-01-11 21:30:47 -0500

    updated auth for github acitons

--- R/auth.R
+++ R/auth.R
@@ -2,12 +2,14 @@
 #' @return the users sysrev token
 get_srtoken = function(){
   res = tryCatch({
-    list(token = keyring::key_get("sysrev","token"),success=T)
+    st    = Sys.getenv("sysrev.token")
+    token = if(nchar(st) > 0){ st }else{ keyring::key_get("sysrev","token") }
+    list(token=token,success=T)
   },error=function(e){
-    list(success=F,error = glue::glue_col(
-    'no default token found. use:
-    1. {cyan `> keyring::key_set("sysrev","token")`}  # to cache a token
-    2. {cyan `> rsr::get_*(3144,token="some-token")`} # to manually set a token for a function'))
+    list(success=F,error = glue::glue_col('no default token found. either
+    1. provide a `token` arg eg {cyan `rsr::get_labels(3144,token="...")`}
+    2. set a default token with {cyan `Sys.setenv(sysrev.token="...")`}
+    3. set a default token with {cyan `keyring::key_set("sysrev","token")`}'))
   })
   if(res$success){ res$token }else{ stop(res$error) }
 }
--- tests/testthat/test-review.R
+++ tests/testthat/test-review.R
@@ -1,3 +1,8 @@
+
 test_that("setting a label value works", {
-  review(pid = 105703,aid = 13458696,lid = "e5772423-7581-4a4a-a0d4-c8c356d2c297",lval = T)
+  pid = 105703
+  aid = 13458696
+  lid = "e5772423-7581-4a4a-a0d4-c8c356d2c297"
+  res = review(pid,aid,lid,lval = T)
+  expect_true(res$setLabels)
 })
