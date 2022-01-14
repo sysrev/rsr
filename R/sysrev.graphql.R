3630e4dd48f487d5126a6b019510a3b71a6c128d
Thomas Luechtefeld <tomluec@gmail.com>
2022-01-14 15:10:25 -0500

    slightly improved error on sysrev.graphql

--- R/sysrev.graphql.R
+++ R/sysrev.graphql.R
@@ -7,7 +7,11 @@ sysrev.graphql <- function(query,token,.url = "https://sysrev.com/graphql"){
 
   res <- httr::content(req, as = "parsed", encoding = "UTF-8")
 
-  if(is.null(res$data)){ jsonlite::prettify(jsonlite::toJSON(res,auto_unbox = T)) |> stop() }
+  if (!is.list(res)){
+    stop("Error response from sysrev graphql, please report to tom@sysrev.com.")
+  }else if (is.null(res$data)) {
+    stop(jsonlite::prettify(jsonlite::toJSON(res, auto_unbox = T)))
+  }
 
   res$data
 }
