ab8c5d243e51378fc09994114d23026a1a4d8810
Thomas Luechtefeld <tomluec@gmail.com>
2022-01-30 22:16:10 -0500

    added clone_sysrev internal function, only works for admins right now

--- R/create.R
+++ R/create.R
@@ -12,9 +12,26 @@ create_sysrev <- function(name,get_if_exists=F,token=get_srtoken()){
   else if(!get_if_exists && sr$exists){
     stop(glue::glue_col("{red {name}} already exists, rerun with {cyan `get_if_exists=T`}?"))
   }
-
+  
   sr$pid
 }
+
+#' Create a new project based on an existing project
+#' @param source.pid project id of the source project
+#' @param target.name name of the new project
+#' @param add.articles import the articles from source project?
+#' @param add.labels import the labels from source project?
+#' @param add.members import the members from source project?
+#' @param add.answers import the answers from source project?
+#' @inheritParams common_params
+#' @keyword internal
+clone_sysrev <- function(source.pid,target.name,add.articles=T,add.labels=T,add.members=F,add.answers=F,token){
+  body = as.list(environment())
+  res = srplumber.post("clone_sysrev",body,token)
+  if(!is.null(res$error)){stop(res$error$message)}
+  res
+}
+
 
 #' create a data source with pubmed ids (pmids)
 #' @inheritParams common_params
